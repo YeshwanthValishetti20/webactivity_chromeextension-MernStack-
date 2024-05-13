3667a6021d251a5c2f0d32b984150e4ad8aeb903
YeshwanthValishetti20 <135430157+YeshwanthValishetti20@users.noreply.github.com>
2024-05-13 10:43:40 +0530

    Delete scripts directory
    Signed-off-by: YeshwanthValishetti20 <135430157+YeshwanthValishetti20@users.noreply.github.com>

--- scripts/zip.ps1
+++
@@ -1,7 +0,0 @@
-$sourceFolder = ".\dist\*"
-$targetFolder = ".\artifacts"
-If (!(test-path -PathType container $targetFolder)) {
-    New-Item -ItemType Directory -Path $targetFolder
-}
-$version = $(node -pe "require('./src/manifest.json').version")
-Compress-Archive -Path $sourceFolder -DestinationPath $targetFolder/web-activity-time-tracker-$version.zip
